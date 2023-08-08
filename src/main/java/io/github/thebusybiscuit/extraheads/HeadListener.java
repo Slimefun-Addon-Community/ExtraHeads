045a3ee5f0147253e4adb31fc88fa901857aa580
ybw0014 <i@guizhanss.com>
2023-08-08 09:36:12 -0400

    feat!: rewrite

--- src/main/java/io/github/thebusybiscuit/extraheads/HeadListener.java
+++ src/main/java/io/github/thebusybiscuit/extraheads/HeadListener.java
@@ -1,5 +1,7 @@
 package io.github.thebusybiscuit.extraheads;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,14 +26,14 @@ public class HeadListener implements Listener {
             return;
         }
 
-        double chance = plugin.getCfg().getDouble("chances." + e.getEntityType().toString());
+        double chance = plugin.getCfg().getDouble("chances." + e.getEntityType());
         Player killer = e.getEntity().getKiller();
 
         if (killer != null && SlimefunUtils.isItemSimilar(killer.getInventory().getItemInMainHand(), SlimefunItems.SWORD_OF_BEHEADING, true)) {
             chance *= plugin.getCfg().getDouble("options.sword-of-beheading-multiplier");
         }
 
-        if (Math.random() * 100 < chance) {
+        if (ThreadLocalRandom.current().nextInt(100) < chance) {
             e.getEntity().getWorld().dropItemNaturally(e.getEntity().getLocation(), plugin.getMobDrops().get(e.getEntityType()));
         }
     }
