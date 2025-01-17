e2372c24b819b9ebe5fa9f5db118ba9ee466828a
ybw0014 <i@guizhanss.com>
2025-01-17 03:40:56 -0800

    chore: lower java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,26 +8,27 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://s01.oss.sonatype.org/content/groups/public/")
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://jitpack.io")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
-    compileOnly("com.github.Slimefun:Slimefun4:e02a0f61d1")
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("com.github.Slimefun:Slimefun4:a1a2abbc47")
     compileOnly("me.clip:placeholderapi:2.11.6")
-    implementation("org.bstats:bstats-bukkit:3.0.3")
+    implementation("org.bstats:bstats-bukkit:3.1.0")
     implementation("com.google.code.findbugs:jsr305:3.0.2")
-    implementation("net.guizhanss:guizhanlib-all:2.0.0-SNAPSHOT")
+    implementation("net.guizhanss:guizhanlib-all:2.2.0")
 }
 
 group = "io.github.thebusybiscuit"
 version = "UNOFFICIAL"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
+    disableAutoTargetJvm()
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 publishing {
